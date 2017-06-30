55337008c793f30186529aa4e2842ca1e7eb8447
William Belle <william.belle@gmail.com>
2017-06-30 20:34:42 +0200

    fix package path

--- src/bin/cli.js
+++ src/bin/cli.js
@@ -20,7 +20,7 @@ var yargs      = require('yargs')
   // Version
   .alias('v', 'version')
   .version(function() {
-    return require('../package').version;
+    return require('../../package').version;
   })
   .describe('v', 'Show version information')
 
