3e20be7387ff7122e49f0142b360a9eadd17f82b
William Belle <william.belle@gmail.com>
2018-01-30 18:50:44 +0100

    ignore ssl errors

--- src/lib/index.js
+++ src/lib/index.js
@@ -9,10 +9,13 @@ var phantom = require('phantomjs-prebuilt');
 
 module.exports = function(url, callback) {
 
+  var phantomjsOption = '--ignore-ssl-errors=true';
   var execFile = require('child_process').execFile;
   var scriptName = __dirname + '/../bin/get-google-analytics-resources.js';
 
-  execFile(phantom.path, [scriptName, url], function(error,stdout) {
+  var execOptions = [phantomjsOption, scriptName, url];
+
+  execFile(phantom.path, execOptions, function(error,stdout) {
     var obj = JSON.parse(stdout);
 
     // Send error message in callback
