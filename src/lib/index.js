94f17f5222384fbdaae8b8284abf4637ae90b313
William Belle <william.belle@gmail.com>
2018-04-06 13:59:03 +0200

    move to eslint

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,19 +3,20 @@
  * See the LICENSE file for more details.
  */
 
-'use strict';
-
 var phantom = require('phantomjs-prebuilt');
+var path = require('path');
 
-module.exports = function(url, callback) {
-
+module.exports = function (url, callback) {
   var phantomjsOption = '--ignore-ssl-errors=true';
   var execFile = require('child_process').execFile;
-  var scriptName = __dirname + '/../bin/get-google-analytics-resources.js';
+  var scriptName = path.join(
+    __dirname,
+    '/../bin/get-google-analytics-resources.js'
+  );
 
   var execOptions = [phantomjsOption, scriptName, url];
 
-  execFile(phantom.path, execOptions, function(error,stdout) {
+  execFile(phantom.path, execOptions, function (error, stdout) {
     var obj = JSON.parse(stdout);
 
     // Send error message in callback
@@ -26,5 +27,4 @@ module.exports = function(url, callback) {
       callback(null, obj);
     }
   });
-
 };
